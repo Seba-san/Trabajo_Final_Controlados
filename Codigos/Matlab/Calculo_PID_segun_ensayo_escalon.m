df2921bbf0df5ab93d8a06c90589b2311b75547f
Seba <sansoni.sebas@gmail.com>
2018-05-28 19:55:48 -0300

    hago un commit para recuperar la medicion respuesta_escalon_motorB180523193004.mat

--- Codigos/Matlab/Calculo_PID_segun_ensayo_escalon.m
+++ Codigos/Matlab/Calculo_PID_segun_ensayo_escalon.m
@@ -1,35 +1,37 @@
-%% Cálculo de PID
-% Este script carga los datos del ensayo al escalón para uno de los motores
+%% Cï¿½lculo de PID
+% Este script carga los datos del ensayo al escalï¿½n para uno de los motores
 % y calcula a partir de estos un controlador PID. Se estima el controlador
-% usando el método de Ziegler Nichols. Para estudiar el controlador
-% diseñado se hace una estimación de la planta y se verifica el efecto del
-% controlador sobre esta estimación de la planta.
-%% Ensayo al escalón - Cargo los datos
+% usando el mï¿½todo de Ziegler Nichols. Para estudiar el controlador
+% diseï¿½ado se hace una estimaciï¿½n de la planta y se verifica el efecto del
+% controlador sobre esta estimaciï¿½n de la planta.
+%% Ensayo al escalï¿½n - Cargo los datos
 clear all;close all;%clc;
-cd('C:\Users\Tania\Documents\ING\Carrera de Grado\Controlados\Trabajo Final con Seba\Git con Seba\Trabajo_Final_Controlados_git\Codigos\Matlab')
+%cd('C:\Users\Tania\Documents\ING\Carrera de Grado\Controlados\Trabajo Final con Seba\Git con Seba\Trabajo_Final_Controlados_git\Codigos\Matlab')
 load('../../Mediciones/respuesta_escalon_motorB180523193004.mat')
+load('../../Mediciones/respuesta_escalon_motorB180523184621.mat')
+%load('..\..\Mediciones\respuesta_escalon_motorB180523193004.mat')
 % load('../../Mediciones/respuesta_escalon_180503210331.mat')
 tiempo=tiempo*1e-6;%Acomodo la unidad del tiempo.
-%% Gráfico de la Respuesta al escalón
+%% Grï¿½fico de la Respuesta al escalï¿½n
 figure(1);
 plot(tiempo,PWMA,tiempo,wA,'.');
-legend('Señal de PWM','Señal de vel ang');
+legend('Seï¿½al de PWM','Seï¿½al de vel ang');
 title('Respuesta del Motor B');
 xlabel('tiempo');ylabel('Vel Ang (rpm) / PWM') %Revisar la unidad!! $
 %% Ajuste del nombre de las variables
-N=length(PWMA);%176;%Corto la señal para sacar valores ruidosos
+N=length(PWMA);%176;%Corto la seï¿½al para sacar valores ruidosos
 entrada=PWMA(1:N);
 salida=wA(1:N);
 tiempo=tiempo(1:N);
 [~,t0]=max(entrada);%Busco el valor donde la entrada pega el salto
-sal0=mean(salida(1:t0-1));%Tomo el valor de corrección como el promedio de 
-%los valores de la salida cuando la entrada vale el mínimo.
+sal0=mean(salida(1:t0-1));%Tomo el valor de correcciï¿½n como el promedio de 
+%los valores de la salida cuando la entrada vale el mï¿½nimo.
 ent0=min(entrada);
 entrada=entrada-ent0;
 salida=salida-sal0;
 figure();
 plot(tiempo,entrada,tiempo,salida,'.');
-legend('Señal de PWM','Señal de vel ang');
+legend('Seï¿½al de PWM','Seï¿½al de vel ang');
 title('Respuesta del Motor B');
 xlabel('tiempo');ylabel('Vel Ang (rpm) / PWM') %Revisar la unidad!! $
 %% PID Discreto con Ziegler - Nichols
@@ -40,13 +42,13 @@ tipo={'P';'PI';'PID'};
 clear A B C D E F;
 for k=1:3
     [Kp,Ki,Kd]=ControlZN(tipo{k},'ZN',entrada,salida,tiempo,0,1);
-    Tf=0;%No sé qué poner en Tf porque ZN no me lo da.
+    Tf=0;%No sï¿½ quï¿½ poner en Tf porque ZN no me lo da.
     control=c2d(tf(pid(Kp,Ki,Kd,Tf)),Ts2,'tustin');
     [A(k,1),B(k,1),C(k,1),D(k,1),E(k,1)]=tf2ctesNano(cell2mat(control.num),cell2mat(control.den),tipo{k});
 end
 ctes=table(A,B,C,D,E,'RowNames',tipo)
 
-%Para pasarle la información a Seba:
+%Para pasarle la informaciï¿½n a Seba:
 ctesP=['{' num2str(ctes.A(1)) ',' num2str(ctes.B(1)) ',' num2str(ctes.C(1)) ',' num2str(ctes.D(1)) ',' num2str(ctes.E(1)) '}'];
 ctesPI=['{' num2str(ctes.A(2)) ',' num2str(ctes.B(2)) ',' num2str(ctes.C(2)) ',' num2str(ctes.D(2)) ',' num2str(ctes.E(2)) '}'];
 ctesPID=['{' num2str(ctes.A(3)) ',' num2str(ctes.B(3)) ',' num2str(ctes.C(3)) ',' num2str(ctes.D(3)) ',' num2str(ctes.E(3)) '}'];
@@ -55,25 +57,25 @@ ctesPID=['{' num2str(ctes.A(3)) ',' num2str(ctes.B(3)) ',' num2str(ctes.C(3)) ',
 controlP=tf([ctes.A(1)],[1],Ts2);
 controlPI=tf([ctes.A(2) ctes.B(2)],[1 -ctes.D(2)],Ts2);
 controlPID=tf([ctes.A(3) ctes.B(3) ctes.C(3)],[1 -ctes.D(3) -ctes.E(3)],Ts2);
-%% Estimación de la Planta (sólo para verificación)
-% Acomodo las muestras para que estén equiespaciadas;
+%% Estimaciï¿½n de la Planta (sï¿½lo para verificaciï¿½n)
+% Acomodo las muestras para que estï¿½n equiespaciadas;
 Fs=2e3;Ts=1/Fs;
 t=min(tiempo):1/Fs:max(tiempo);
 w_interp = interp1(tiempo,salida,t); % Interpola lo datos de salida
 inicia=find(entrada==max(entrada),1);
-%Tania: El primer valor al que la entrada es máxima es cuando el escalón
-%empieza. Acá la idea es buscar ese valor de tiempo en el nuevo vector para
-%después generar el vector de la señal de entrada.
+%Tania: El primer valor al que la entrada es mï¿½xima es cuando el escalï¿½n
+%empieza. Acï¿½ la idea es buscar ese valor de tiempo en el nuevo vector para
+%despuï¿½s generar el vector de la seï¿½al de entrada.
 % Busca cuando empezo el pulso:
 inicia_seg=find(abs(tiempo(inicia)-t)==min(abs(tiempo(inicia)-t)));
 pwm_interp=zeros(1,length(t));
 pwm_interp(inicia_seg:length(t))=max(entrada);pwm_interp(1:(inicia_seg-1))=min(entrada);
 % figure(2);subplot(211);stem(tiempo,salida);hold on; plot(t,w_interp,'.');
-% title('Interpolacion Lineal de w-salida');legend('Muestras Originales','Interpolación');
+% title('Interpolacion Lineal de w-salida');legend('Muestras Originales','Interpolaciï¿½n');
 % subplot(212);stem(tiempo,entrada);hold on; plot(t,pwm_interp,'.');
-% title('Interpolacion Lineal de pwm-entrada');legend('Muestras Originales','Interpolación');
+% title('Interpolacion Lineal de pwm-entrada');legend('Muestras Originales','Interpolaciï¿½n');
 
-% Estimación de la Planta:
+% Estimaciï¿½n de la Planta:
 data = iddata(w_interp',pwm_interp',Ts);
 np=3;%Le indico el nro de polos
 nz=[];%Le indico el nro de ceros
@@ -82,13 +84,13 @@ sys = tfest(data,np,nz,iodelay,'Ts',data.Ts);
 syscont = tfest(data,np,nz,[]);
 % Cambio el tiempo de muestreo del sistema:
 sys2=d2d(sys,Ts2);
-% Acomodo la señal de entrada a la nueva frec de muestreo:
+% Acomodo la seï¿½al de entrada a la nueva frec de muestreo:
 t2=linspace(min(t),max(t),(max(t)-min(t))/Ts2);
-[~,indice]=max(pwm_interp);%Busco el punto donde la señal cambia
-%Busco el índice del valor de t2 más próximo al valor de tiempo donde se da
-%el mínimo de pwm_interp:
+[~,indice]=max(pwm_interp);%Busco el punto donde la seï¿½al cambia
+%Busco el ï¿½ndice del valor de t2 mï¿½s prï¿½ximo al valor de tiempo donde se da
+%el mï¿½nimo de pwm_interp:
 [~,indice]=min(abs(t2-t(indice)));
-%Armo a mano el vector de entrada para que sea un escalón perfecto con
+%Armo a mano el vector de entrada para que sea un escalï¿½n perfecto con
 %cambio en el tiempo encontrado antes:
 pwm2(indice:length(t2))=max(pwm_interp)*ones(1,length(t2)-indice+1);
 pwm2(1:indice-1)=min(pwm_interp)*ones(1,indice-1);
@@ -101,14 +103,14 @@ title('Respuesta del Sistema Orignal y del Estimado');
 legend('Rpta del Sist Original','Rpta del Sist Estimado','Rpta del Sist Estimado con Fs reducida');
 
 % figure();step(syscont);hold on;step(sys);step(sys2);
-% title('Respuesta al Escalón');
-% legend('Versión Contínua','Versión discreta 1','Versión discreta 2');
+% title('Respuesta al Escalï¿½n');
+% legend('Versiï¿½n Contï¿½nua','Versiï¿½n discreta 1','Versiï¿½n discreta 2');
 %% Diagrama de polos y ceros para los sistemas estimados y los controladores
 % figure();subplot(121);iopzmap(syscont);subplot(122);iopzmap(sys);hold on;iopzmap(sys2);
 figure();subplot(121);iopzmap(syscont);subplot(122);iopzmap(c2d(syscont,Ts2,'tustin'));
 figure();subplot(121);iopzmap(controlPI);subplot(122);iopzmap(d2c(controlPI,'tustin'));
 figure();subplot(121);iopzmap(controlPID);subplot(122);iopzmap(d2c(controlPID,'tustin'));
-%% Desempeño del controlador para la planta estimada
+%% Desempeï¿½o del controlador para la planta estimada
 %Armo los sistemas realimentados:
 sysControladoP= feedback(series(d2c(controlP,'tustin'),syscont),1);
 sysControladoPI= feedback(series(d2c(controlPI,'tustin'),syscont),1);
@@ -121,6 +123,6 @@ sysControladoPID= feedback(series(d2c(controlPID,'tustin'),syscont),1);
 % title('Respuesta del Sistema Controlado');
 % legend('Control P','Control PI','Control PID');
 
-%Calculo las respuestas al escalón de los sistemas realimentados:
+%Calculo las respuestas al escalï¿½n de los sistemas realimentados:
 figure();step(sysControladoP);hold on;step(sysControladoPI);%step(sysControladoPID);
 legend('Control P','Control PI','Control PID');
