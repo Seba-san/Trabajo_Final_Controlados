ec4479754c9b0a61b8c26f4007947f39c9071898
Seba <sansoni.sebas@gmail.com>
2018-06-05 14:28:55 -0300

    Se cargaron los datos y se estimo el sistema. Se armo el sistema completo y se calculo el PI, tiene datos  coherentes

--- Codigos/Matlab/Matlab_Seba/Levantando_datos_medidos.m
+++ Codigos/Matlab/Matlab_Seba/Levantando_datos_medidos.m
@@ -31,14 +31,22 @@ plot(t,dW);ylabel('delta RPM estimadas')
 yyaxis right
 plot(t,beta);ylabel('beta medido')
 dW1=dW;beta1=beta;
+%%
 % Otro ensayo
 
 %load('../../Mediciones/180603192250resp_escalon_sistema_total.mat')
 load('../../Mediciones/180603192511resp_escalon_sistema_total.mat')
+%load('../../Mediciones/180604194417ensayo_a_vel_cte_con_PI.mat') % Solo
+%control
+%load('../../Mediciones/180604195315resp_escalon_sistema_total.mat') % con
+%bolantazo
+%load('../../Mediciones/180604195905resp_escalon_sistema_total.mat') % Con bolantazo
+try
 [indice]=find(beta==3);t=t(1:(indice(1)-1));beta=beta(1:(indice(1)-1));
 wA=wA(1:(indice(1)-1));wB=wB(1:(indice(1)-1));
+end
 dW=wB-wA;
-sin_control_t=50;
+tsc=50;
 figure(2)
 subplot(311)
 yyaxis left
@@ -61,17 +69,46 @@ plot(t,beta,t(50),beta(50),'o');ylabel('beta medido')
 
 % Separo  Datos sin control y con control
 
-dWsc=dW(51:end);dWcc=dW(1:50);
+dWsc=dW(51:end);dWcc=dW(1:50);tcc=t(1:50);tsc=t(51:end);
 betasc=beta(51:end);betacc=beta(1:50);
 figure(3)
 yyaxis left
-plot(dWsc)
+plot(tsc,dWsc) ; ylabel('dW [RPM]')
 yyaxis right
-plot(betasc)
+plot(tsc,betasc); ylabel('beta')
 title('Sin control')
 figure(4)
 yyaxis left
-plot(dWcc)
+plot(tcc,dWcc); ylabel('dW [RPM]')
 yyaxis right
-plot(betacc)
+plot(tcc,betacc); ylabel('beta')
 title('Con control')
+
+
+
+%%
+ls ../../Mediciones
+%%
+% sys_t=sysA_c;
+% A=sys_t.A;B=sys_t.B;C=sys_t.C;D=sys_t.D;E=sys_t.E;
+% [b,a]=ss2tf(A,B,C,D,E,1);sis=tf(a,b)
+
+sys=sysA;
+sys_=zpk([],[-1/sys.Tp1 -1/sys.Tp2],sys.Kp/(sys.Tp1*sys.Tp2));%,'OutputDelay',sys.Td);
+C=Ca;
+Kp=C.Kp;Ki=C.Ki;Kd=C.Kd;Tf=0;
+control=tf(pid(Kp,Ki,Kd,Tf)); 
+% Sin delay
+sys_cm=control*sys_*inv(1+control*sys_);
+% Con delay
+%sys_cmd=control*sysA*inv(1+control*sysA)
+
+sys_t=sys_cm*sys_cinematico
+
+
+
+
+
+
+
+
