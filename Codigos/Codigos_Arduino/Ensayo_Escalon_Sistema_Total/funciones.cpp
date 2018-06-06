cfe478b6f546297ecdca9ae8850416e43e9cef48
Tania <ferreyra.1992@gmail.com>
2018-06-06 18:09:26 -0300

    commit previo a ensayos escalón

--- Codigos/Codigos_Arduino/Ensayo_Escalon_Sistema_Total/funciones.cpp
+++ Codigos/Codigos_Arduino/Ensayo_Escalon_Sistema_Total/funciones.cpp
@@ -30,7 +30,7 @@ void PID_total(void){//PID del sistema en su conjunto
     }
   //errorBeta[2]=((float)(0)-beta);//El ángulo deseado es siempre 0  
   //errorBeta[2]=((float)(0.052359878)-beta);//El ángulo deseado no es 0 porque el sensor nunca mide cero
-  errorBeta[2]=((float)(0.074176493)-beta);//El ángulo deseado no es 0 porque el sensor nunca mide cero
+  errorBeta[2]=((float)(-0.039269908)-beta);//El ángulo deseado no es 0 porque el sensor nunca mide cero
   dw[2]=Parametros[0]*errorBeta[2]+Parametros[1]*errorBeta[1]+Parametros[2]*errorBeta[0]+Parametros[3]*dw[1]+Parametros[4]*dw[0];
   if (dw[2]>windup_top_dw){dw[2]=windup_top_dw;} //Cambiar nombres de windup_top y _bottom $$$$$
   if (dw[2]<windup_bottom_dw){dw[2]=windup_bottom_dw;}
