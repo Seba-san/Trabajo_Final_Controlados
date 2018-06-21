6d0fb0b117d27601aa45ec9b70eab9b91680053f
Seba <sansoni.sebas@gmail.com>
2018-06-21 08:47:44 -0300

    Se esta realizando una adaptacion en la comunicacion para poder utilizar los modulitos de bluetooth, ya que posee un canal real de comunicacion de 8000 bps; tiene un buffer muy grande lo que permite enviar info hacia el modulo de hasta 115200 bps.

---
+++ Codigos/Matlab/Matlab_Seba/DatoRx_rf.m
@@ -0,0 +1,23 @@
+function dato=DatoRx_rf(s)
+
+% Supongo [inicio][angulo][RPMA][RPMB]
+% suponer inicio=0xFF
+% hay comprecion de datos: RPMA=dato_rx*1000/255 ; Se puede suponer otra,
+% por ejemplo: RPMA=dato_rx*delta/255 + x0; En el transmisor tendria que
+% ser asi: dato_tx=freqA*255/1000; y con la otra comprecion es:
+% dato_tx=(freqA -x0)*255/delta; donde delta=valor_maximo - valor_minimo;
+CantdeBytes=3; 
+inicio=255;
+a=0;
+while a~=inicio
+a=LeerSerial(s);
+end
+dato=zeros(1,CantdeBytes);
+for i=1:CantdeBytes-1
+dato(i)=LeerSerial(s);
+end
+% Descomprecion
+dato(2)=dato(2)*1000/255;
+dato(3)=dato(3)*1000/255;
+
+end
